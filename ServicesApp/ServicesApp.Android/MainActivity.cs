31d8df79fffbba00deea7764390bd9d1a318964d
Praveen <Praveen@DESKTOP-DD11LNH>
2021-10-06 15:36:52 +0530

    Add project files.

---
+++ ServicesApp/ServicesApp.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Content;
+
+namespace ServicesApp.Droid
+{
+    [Activity(Label = "ServicesApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            MessagingCenter.Unsubscribe<string>(this, "Key");
+            MessagingCenter.Subscribe<string>(this, "Key", (value) =>
+            {
+                if (value == "1")
+                {
+                    StartService(new Intent(this, typeof(BackgroundServices)));
+                }
+                else
+                {
+                    StopService(new Intent(this, typeof(BackgroundServices)));
+                }
+            });
+            
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ ServicesApp/ServicesApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ServicesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MessagingCenter.Unsubscribe<string>(this, "Keytocounter");
+            MessagingCenter.Subscribe<string>(this, "Keytocounter", (value) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    counter.Text = value;
+                });
+            });
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            MessagingCenter.Send<string>("1", "Key");
+        }
+        private void Stop(object sender, EventArgs e)
+        {
+            MessagingCenter.Send<string>("0", "Key");
+        }
+    }
+}
